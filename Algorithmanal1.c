f2a9f8839794f0af6d1886427fb3e88c2b2ad789
shraditya <56548373+shraditya@users.noreply.github.com>
2021-04-15 15:00:19 +0530

    Update Algorithmanal1.c

--- Algorithmanal1.c
+++ Algorithmanal1.c
@@ -89,9 +89,7 @@ int takeInput()
       t = autofill(arr,size);
         
         printf("Time took to autofill the array is %ld microseconds\n\n",t);
-        printf("Elements given in the list are \n[");
-                for(int i=0;i<size;i++){printf("%d,",arr[i]);}
-                printf("]\n");
+        printf("Elements in array are decending numbers from [%d - 1]",size);
         printf("\n----\n");} 
     else{
       printf("size : %d --> too inappropriate size to give the appropriate result required for analysis of algorithm.\n\n",size);
@@ -161,7 +159,7 @@ double insertionSort(int arr[], int n)
         }
         arr[j + 1] = key;
     }
-    t = t - clock();
+    t = clock() -t;
     return t;
 }
 void merge(int arr[], int l, int m, int r)
@@ -229,6 +227,74 @@ double mergeSort(int arr[], int l, int r)
     t = clock()-t;
     return t;
 }
+int getMax(int arr[], int n)
+{
+    int mx = arr[0];
+    for (int i = 1; i < n; i++)
+        if (arr[i] > mx)
+            mx = arr[i];
+    return mx;
+}
+  
+// A function to do counting sort of arr[] according to
+// the digit represented by exp.
+void countSort(int arr[], int n, int exp)
+{
+    int output[n]; // output array
+    int i, count[10] = {0};
+  
+    // Store count of occurrences in count[]
+    for (i = 0; i < n; i++)
+        count[ (arr[i]/exp)%10 ]++;
+  
+    // Change count[i] so that count[i] now contains actual
+    //  position of this digit in output[]
+    for (i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+  
+    // Build the output array
+    for (i = n - 1; i >= 0; i--)
+    {
+        output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+        count[ (arr[i]/exp)%10 ]--;
+    }
+  
+    // Copy the output array to arr[], so that arr[] now
+    // contains sorted numbers according to current digit
+    for (i = 0; i < n; i++)
+        arr[i] = output[i];
+}
+  
+// The main function to that sorts arr[] of size n using 
+// Radix Sort
+double radixsort(int arr[], int n)
+{
+    // Find the maximum number to know number of digits
+    clock_t t;
+    t = clock();
+    int m = getMax(arr, n);
+  
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; m/exp > 0; exp *= 10)
+        countSort(arr, n, exp);
+    t = clock()-t;
+    return t;
+}
+void sortDetails(int i)
+{
+    
+}
+
+void sortBuffer(int arr[msize],int size)
+{
+    printf("time taken by Merge Sort: %f\n\n",mergeSort(arr,0,size-1));
+    printf("time taken by Bubble Sort: %f\n\n",bubbleSort(arr,size));
+    printf("time taken by Selection Sort: %f\n\n",selectionSort(arr,size));
+    printf("time taken by Insertion Sort: %f\n\n",insertionSort(arr,size));
+    printf("time taken by Radix Sort: %f\n\n",radixsort(arr,size));
+}
 
 
 int main() {
@@ -237,7 +303,7 @@ int main() {
   printf(" \nNote : For better results please enter the size which is a little larger than human intuition and which is under computer powers! A good range of size lies between 100 to 100,000\n\n");
   
   int size = takeInput();
-  printf("time taken : %f\n\n",mergeSort(arr,0,size-1));
+  sortBuffer(arr,size);
   printArray(&arr,size);
   
   
