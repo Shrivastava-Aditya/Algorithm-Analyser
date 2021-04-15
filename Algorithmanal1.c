7b081b786e6f89f1055ddcfec30aae201f973979
shraditya <56548373+shraditya@users.noreply.github.com>
2021-04-15 14:17:00 +0530

    Update Algorithmanal1.c

--- Algorithmanal1.c
+++ Algorithmanal1.c
@@ -106,8 +106,6 @@ void swap(int *xp, int *yp)
     *xp = *yp;
     *yp = temp;
 }
-
-  
 double selectionSort(int arr[], int n)
 {
     clock_t t;
@@ -145,6 +143,92 @@ double bubbleSort(int arr[], int n)
    return t;
    
 }
+double insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    clock_t t;
+    t = clock();
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+ 
+        /* Move elements of arr[0..i-1], that are
+          greater than key, to one position ahead
+          of their current position */
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    t = t - clock();
+    return t;
+}
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    /* create temp arrays */
+    int L[n1], R[n2];
+ 
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+ 
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+ 
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+double mergeSort(int arr[], int l, int r)
+{
+    clock_t t;
+    t = clock();
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+ 
+        merge(arr, l, m, r);
+    }
+    t = clock()-t;
+    return t;
+}
 
 
 int main() {
@@ -153,7 +237,7 @@ int main() {
   printf(" \nNote : For better results please enter the size which is a little larger than human intuition and which is under computer powers! A good range of size lies between 100 to 100,000\n\n");
   
   int size = takeInput();
-  printf("time taken : %f\n\n",bubbleSort(&arr,size));
+  printf("time taken : %f\n\n",mergeSort(arr,0,size-1));
   printArray(&arr,size);
   
   
